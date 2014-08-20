5b0a307fdf3de6f45fc283e435bc14a832d8f4d1
Luis Fernando Chimelo Ruiz <ruiz.ch@gmail.com>
2014-08-20 13:13:39 -0300

    Update c50_python.py

--- c50_python.py
+++ c50_python.py
@@ -9,3 +9,5 @@ d={'r':r_n,'g':g_n,'b':b_n}
 dataf = robject.DataFrame(d)
 ad=c50.C5_0(dataf,classes,triasl=10,control=(c50.C5_0Control(minCases = 2, CF = 0.2)))
 #Ler ad
+base = importr('base')
+print(base.summary(ad))
