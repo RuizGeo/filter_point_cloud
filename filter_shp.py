eb68a247f5c677d4c339044bf37c959b445381de
Luis Fernando Chimelo Ruiz <ruiz.ch@gmail.com>
2014-09-16 14:46:35 -0300

    Update filter_shp.py

--- filter_shp.py
+++ filter_shp.py
@@ -10,10 +10,21 @@ from qgis.core import *
 class filterSHP:
 
     '''
-    Create decision tree C50
-    Input class (list) and trein (array numpy)
+    Filter points clould
+    Input conditional (string), datas (array numpy), fields (list) and array ID points
     Output ...
     '''
-    def __init__(self,conditional = -1, trein = -1):
-        self.clas = clas
-        self.trein = trein
+    def __init__(self,conditional = -1, datas = -1, fields=-1, array_id=-1):
+        self.cond = conditional
+        self.datas = datas
+        self.fields = fields
+        self.array_id = array_id
+    def selectIDs(self):
+        #criar as variaveis para cada atributo (coluna)
+        for i, v in enumerate(self.fields):
+            exec(v+'=self.datas[:,i]')
+        #avaliar as condicionais, como resultado gera um array np 0 e 1
+        cond_eval=eval(self.cond)
+        #selecionar no array apenas os valores com classe igual a 1
+        id_selec = self.array_id[np.where(cond_eval)]
+        return id_selec
