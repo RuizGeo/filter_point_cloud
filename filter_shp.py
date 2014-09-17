b918b1f55a4be2d54f4211529673331d8276dc86
Luis Fernando Chimelo Ruiz <ruiz.ch@gmail.com>
2014-09-16 23:24:05 -0300

    Update filter_shp.py

--- filter_shp.py
+++ filter_shp.py
@@ -14,11 +14,12 @@ class filterSHP:
     Input conditional (string), datas (array numpy), fields (list) and array ID points
     Output ...
     '''
-    def __init__(self,conditional = -1, datas = -1, fields=-1, array_id=-1):
+    def __init__(self,conditional = -1, datas = -1, fields=-1, array_id=-1, layer=-1):
         self.cond = conditional
         self.datas = datas
         self.fields = fields
         self.array_id = array_id
+        self.layer=layer
     def selectIDs(self):
         #criar as variaveis para cada atributo (coluna)
         for i, v in enumerate(self.fields):
@@ -26,5 +27,14 @@ class filterSHP:
         #avaliar as condicionais, como resultado gera um array np 0 e 1
         cond_eval=eval(self.cond)
         #selecionar no array apenas os valores com classe igual a 1
-        id_selec = self.array_id[np.where(cond_eval)]
+        self.id_selec = self.array_id[np.where(cond_eval)]
+        #Delete features of conditional
+        self.layer.dataProvider().deleteFeatures(id_selec)
+        #create spatial index
+        index = QgsSpatialIndex()
+        #obter features
+        layer_feat = self.getFeatures()
+        #inserir features para gerar os grupos de distancias
+        for f in layer_feat :
+            index.insertFeature(f)
         return id_selec
